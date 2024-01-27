e5339fcb1c32bd13dbfc62963569c24c1c1e4653
evandvance <evandvance@gmail.com>
2024-01-27 15:36:13 -0600

    Added math page

--- routes/math.js
+++ routes/math.js
@@ -1,17 +1,26 @@
 const express = require("express");
 const router = express.Router();
 
+router.get('/', (req, res) => {
+    res.render('math', {title: "Math", message:"Fill out the form"})
+})
+
 router.get('/:num1/:op/:num2', (req, res)=> {
-    const opp = req.params.op;
+    const op = req.params.op;
     //Forget error handling. Errors dont happen if you use things as intended.
     const num1 = parseInt(req.params.num1);
     const num2 = parseInt(req.params.num2);
 
-    const answer = handleOpperation(opp,num1,num2);
+    const answer = handleOpperation(op,num1,num2);
 
-    const message = `${num1} ${opp} ${num2} = ${answer}`
+    const message = `${num1} ${op} ${num2} = ${answer}`
     res.render("math",{title:"Math", message});
 });
+
+router.post('/', (req,res) => {
+    console.log(req.body)
+    res.redirect(`/math/${req.body.num1}/${req.body.op}/${req.body.num2}`)
+})
 
 function handleOpperation(opp,num1,num2) {
     switch(opp){
