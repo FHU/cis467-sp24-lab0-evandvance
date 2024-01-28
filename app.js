79fea5e5a4c2a6efbb7b85fdb5fdd5c4f1df7a37
evandvance <evandvance@gmail.com>
2024-01-28 14:02:01 -0600

    Added 404 handling

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-//TODO Break code into smaller files
 const express = require('express');
 const app = express();
 
@@ -9,6 +8,7 @@ const pandorasboxRoute = require("./routes/pandorasbox");
 const PORT = process.env.PORT || "3000";
 
 app.set('view engine', 'ejs');
+//This Line is for allowing posts from forms to be handled correctly
 app.use(express.urlencoded({ extended: true }))
 
 app.use('/greet', greetRoute);
@@ -17,6 +17,11 @@ app.use('/pandorasbox', pandorasboxRoute);
 
 app.get("/", (req, res) => {
     res.sendFile(`${__dirname}/public/index.html`)
+})
+
+//404 Handling (Always leave at end of routes)
+app.get("*", (req,res) => {
+    res.status(404).sendFile(`${__dirname}/public/404.html`)
 })
 
 app.listen(PORT, () => console.log(`App is running on port ${PORT}`));
