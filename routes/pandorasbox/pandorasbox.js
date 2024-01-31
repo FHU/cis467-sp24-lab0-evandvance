31f15df97c296fb4491bf65b56309cc41ad74b02
evandvance <evandvance@gmail.com>
2024-01-31 14:27:04 -0600

    Added call to dad joke api

--- routes/pandorasbox/pandorasbox.js
+++ routes/pandorasbox/pandorasbox.js
@@ -2,11 +2,25 @@ const express = require("express");
 const router = express.Router();
 const facts = require("./facts.json")
 
-router.get('/', (req, res)=> {
-    const message = facts[Math.floor(Math.random() * facts.length)].fact;
+router.get('/', async (req, res) => {
 
-    res.render('pandorasbox', {title: "Pandora's Box", message} );
+    const selector = Math.floor(Math.random() * 2);
+    const messages = [await dadJoke(), facts[Math.floor(Math.random() * facts.length)].fact];
 
+    res.render('pandorasbox', {title: "Pandora's Box", message: messages[selector]} );
 });
+
+async function dadJoke() {
+
+    let joke = "";
+
+    await fetch("https://icanhazdadjoke.com/", {
+            headers: {"Accept": "application/json"}
+        })
+        .then(res => res.json())
+        .then(data => joke = data);
+
+    return joke["joke"];
+};
 
 module.exports = router;
